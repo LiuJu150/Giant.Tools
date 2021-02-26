cbf72618813bc1ceac7e865a1b9fc7d1e03507fe
刘巨 <liuju150@outlook.com>
2021-02-26 16:06:30 +0800

    PNG导出

--- PdfImage/Program.cs
+++ PdfImage/Program.cs
@@ -3,8 +3,11 @@ using PdfSharp.Pdf.Advanced;
 using PdfSharp.Pdf.IO;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ConsoleApp6
@@ -78,7 +81,51 @@ namespace ConsoleApp6
             count++;
             int width = image.Elements.GetInteger(PdfImage.Keys.Width);
             int height = image.Elements.GetInteger(PdfImage.Keys.Height);
-            int bitsPerComponent = image.Elements.GetInteger(PdfImage.Keys.BitsPerComponent);
+            //int bitsPerComponent = image.Elements.GetInteger(PdfImage.Keys.BitsPerComponent);//这个不准确
+
+            var canUnfilter = image.Stream.TryUnfilter();
+            byte[] decodedBytes;
+
+            if (canUnfilter)
+                decodedBytes = image.Stream.Value;
+            else
+            {
+                PdfSharp.Pdf.Filters.FlateDecode flate = new PdfSharp.Pdf.Filters.FlateDecode();
+                decodedBytes = flate.Decode(image.Stream.Value);
+            }
+
+            int bitsPerComponent = 0;
+            while (decodedBytes.Length - ((width * height) * bitsPerComponent / 8) != 0)
+            {
+                bitsPerComponent++;
+            }
+
+            PixelFormat pixelFormat;
+            switch (bitsPerComponent)
+            {
+                case 1: pixelFormat = PixelFormat.Format1bppIndexed; break;
+                case 8: pixelFormat = PixelFormat.Format8bppIndexed; break;
+                case 16: pixelFormat = PixelFormat.Format16bppArgb1555; break;
+                case 24: pixelFormat = PixelFormat.Format24bppRgb; break;
+                case 32: pixelFormat = PixelFormat.Format32bppArgb; break;
+                case 64: pixelFormat = PixelFormat.Format64bppArgb; break;
+                default: throw new Exception("Unknown pixel format " + bitsPerComponent);
+            }
+
+            decodedBytes = decodedBytes.Reverse().ToArray();
+
+            Bitmap bmp = new Bitmap(width, height, pixelFormat);
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+            int length = (int)Math.Ceiling(width * (bitsPerComponent / 8.0));
+            for (int i = 0; i < height; i++)
+            {
+                int offset = i * length;
+                int scanOffset = i * bmpData.Stride;
+                Marshal.Copy(decodedBytes, offset, new IntPtr(bmpData.Scan0.ToInt32() + scanOffset), length);
+            }
+            bmp.UnlockBits(bmpData);
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            bmp.Save(Path.Combine(dirPath, $"{count}.png"), ImageFormat.Png);
 
             // TODO: You can put the code here that converts vom PDF internal image format to a Windows bitmap
             // and use GDI+ to save it in PNG format.

773c72597606213502648a4628c0689a987d376d
Giant <liuju150@outlook.com>
2021-06-08 10:44:02 +0800

    条码生成器

---
+++ QRCode/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace QRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numWidth = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.rdoBarcode = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdoQRCode = new System.Windows.Forms.RadioButton();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox
+            // 
+            this.imgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgBox.Location = new System.Drawing.Point(12, 109);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(403, 345);
+            this.imgBox.TabIndex = 0;
+            this.imgBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "条码内容:";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(78, 10);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(225, 23);
+            this.txtCode.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "条码宽度:";
+            // 
+            // numWidth
+            // 
+            this.numWidth.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numWidth.Location = new System.Drawing.Point(78, 39);
+            this.numWidth.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numWidth.Name = "numWidth";
+            this.numWidth.Size = new System.Drawing.Size(77, 23);
+            this.numWidth.TabIndex = 4;
+            this.numWidth.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "条码高度:";
+            // 
+            // numHeight
+            // 
+            this.numHeight.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numHeight.Location = new System.Drawing.Point(226, 39);
+            this.numHeight.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(77, 23);
+            this.numHeight.TabIndex = 6;
+            this.numHeight.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // rdoBarcode
+            // 
+            this.rdoBarcode.AutoSize = true;
+            this.rdoBarcode.Checked = true;
+            this.rdoBarcode.Location = new System.Drawing.Point(78, 68);
+            this.rdoBarcode.Name = "rdoBarcode";
+            this.rdoBarcode.Size = new System.Drawing.Size(75, 21);
+            this.rdoBarcode.TabIndex = 7;
+            this.rdoBarcode.TabStop = true;
+            this.rdoBarcode.Text = "Barcode";
+            this.rdoBarcode.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "条码类型:";
+            // 
+            // rdoQRCode
+            // 
+            this.rdoQRCode.AutoSize = true;
+            this.rdoQRCode.Location = new System.Drawing.Point(161, 68);
+            this.rdoQRCode.Name = "rdoQRCode";
+            this.rdoQRCode.Size = new System.Drawing.Size(75, 21);
+            this.rdoQRCode.TabIndex = 9;
+            this.rdoQRCode.TabStop = true;
+            this.rdoQRCode.Text = "QRCode";
+            this.rdoQRCode.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(309, 10);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(106, 77);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "生成";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 466);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.rdoQRCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rdoBarcode);
+            this.Controls.Add(this.numHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "条码生成器";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.RadioButton rdoBarcode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rdoQRCode;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ QRCode/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var code = this.txtCode.Text;
+            var width = this.numWidth.Value;
+            var height = this.numHeight.Value;
+
+            var writer = new BarcodeWriter();
+            writer.Format = this.rdoBarcode.Checked ? BarcodeFormat.CODE_128 : BarcodeFormat.QR_CODE;
+            var options = new ZXing.Common.EncodingOptions()
+            {
+                Width = (int)width,
+                Height = (int)height,
+                Margin = 2,
+                PureBarcode = false
+            };
+            writer.Options = options;
+            var img = writer.Write(code);
+            this.imgBox.Image = img;
+        }
+    }
+}
